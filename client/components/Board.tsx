487fe23775a932cde4605946ee7ebbabe45ea5c2
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-15 10:22:43 +1200

    Fixed - test array is being properlly filled with values

--- client/components/Board.tsx
+++ client/components/Board.tsx
@@ -1,7 +1,7 @@
 import Cell from './Cell'
 
 export default function Board({ size }: { size: number }) {
-  const testArr = new Array(size * size)
+  const testArr = new Array(size * size).fill(0)
   return testArr.map((arr, i) => {
     return <Cell key={i} />
   })
