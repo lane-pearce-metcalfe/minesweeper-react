b3169f1b10b5d066422b41d638e74435a6ea0f0a
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-15 11:03:40 +1200

    Updated - Styling for cells

--- client/components/Cell.tsx
+++ client/components/Cell.tsx
@@ -1,7 +1,22 @@
 export default function Cell() {
   return (
-    <div>
-      <p>1</p>
+    <div
+      style={{
+        backgroundColor: 'lightgray',
+        color: 'blue',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: '1.6rem',
+        boxSizing: 'border-box',
+        borderBottom: '3px solid black',
+        borderRight: '3px solid black',
+        borderTop: '3px solid white',
+        borderLeft: '3px solid white',
+      }}
+    >
+      <p style={{ margin: '0' }}>1</p>
     </div>
   )
 }
