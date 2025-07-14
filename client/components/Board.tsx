09fb4db1bbd7a027704c99e12a102dbe36da65a5
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-15 10:39:48 +1200

    Added - function in the Board component to create an array with rows that include objects that have values such as isMine, isRevealed, isFlagged, and nearbyMines as well as the interface and types for the array and objects

--- client/components/Board.tsx
+++ client/components/Board.tsx
@@ -1,6 +1,31 @@
 import Cell from './Cell'
 
+interface CellData {
+  isMine: boolean
+  isRevealed: boolean
+  isFlagged: boolean
+  nearbyMines: boolean
+}
+
+type Board = CellData[][]
+
 export default function Board({ size }: { size: number }) {
+  function createBoard() {
+    const newBoard: Board = []
+    for (let row = 0; row < size; row++) {
+      newBoard[row] = []
+      for (let col = 0; row < size; col++) {
+        newBoard[row][col] = {
+          isMine: false,
+          isRevealed: false,
+          isFlagged: false,
+          nearbyMines: false,
+        }
+      }
+    }
+    return newBoard
+  }
+
   const testArr = new Array(size * size).fill(0)
   return testArr.map((arr, i) => {
     return <Cell key={i} />
